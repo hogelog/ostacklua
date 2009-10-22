cc0460d56a81ea6a159f4415967600ffdf14edd6
Komuro Sunao <konbu.komuro@gmail.com>
2009-10-22 13:57:30 +0900

    remove unused code

--- src/lstate.c
+++ src/lstate.c
@@ -5,6 +5,7 @@
 */
 
 
+#include <stdio.h>
 #include <stddef.h>
 
 #define lstate_c
@@ -138,12 +139,6 @@ static void preinit_state (lua_State *L, global_State *g) {
   setnilvalue(gt(L));
 }
 
-#include <stdio.h>
-#define settimeull(ull, spec) { \
-  (ull) = (spec).tv_sec; \
-  (ull) *= 1000000000; \
-  (ull) += (spec).tv_nsec; \
-}
 static void close_state (lua_State *L) {
   global_State *g = G(L);
   unsigned long long lua_end;
@@ -157,7 +152,7 @@ static void close_state (lua_State *L) {
   lua_assert(g->totalbytes == sizeof(LG));
   (*g->frealloc)(g->ud, fromstate(L), state_size(LG), 0);
   lua_end = getmicrosec();
-  fprintf(stderr, "## execution: %.4f s, gc: %.4f s\n", (lua_end - g->lua_start)/1000000.0, g->gctime/1000000.0);
+  fprintf(stderr, "## execution: %.6f s, gc: %.6f s\n", (lua_end - g->lua_start)/1000000.0, g->gctime/1000000.0);
 }
 
 

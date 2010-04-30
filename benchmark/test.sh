51649c10e2d75f0ae9631347e0d8cdb95193595f
Komuro Sunao <konbu.komuro@gmail.com>
2010-04-30 17:03:23 +0900

    modify benchmarks

--- benchmark/test.sh
+++ benchmark/test.sh
@@ -8,10 +8,12 @@ test() {
   $1 $2 pidigits.lua 10000
   $1 $2 binarytrees.lua 16
   $1 $2 fasta.lua 25000000
-  $1 $2 ao.lua
+  $1 $2 ao.lua 0
   $1 $2 spectralnorm.lua 5500
   $1 $2 nbody.lua 50000000
   $1 $2 mandelbrot.lua 16000
   $1 $2 fannkuch.lua 12
 }
-test time $LUA >/dev/null
+echo '## start test'
+test time $LUA 2>&1 >/dev/null
+echo '## end test'
